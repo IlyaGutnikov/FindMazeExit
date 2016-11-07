6aa19628d75737e50e53780bc81f8c30e8a5946d
Ilya Gutnikov <ilyagutnikov93@gmail.com>
2016-11-07 20:16:04 +0300

    Обход есть, но он жрет много ресурсов.

--- FindMazeExit/Assets/SpawnCheck.cs
+++ FindMazeExit/Assets/SpawnCheck.cs
@@ -1,16 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class SpawnCheck : MonoBehaviour {
 
 	public GameObject checkObject;
 
 	public bool canSpawnCheck = true;
+	Vector2 boxSize;
 
 	public GameObject spawnedObject;
 	// Use this for initialization
 	void Start () {
 		Debug.Log("Into spawn check");
+	
+		boxSize = checkObject.transform.localScale;
+		Debug.Log ("BoxSize " + boxSize);
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
@@ -28,14 +33,22 @@ public class SpawnCheck : MonoBehaviour {
 	}
 		
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 
 		if (canSpawnCheck == true) {
 
-			Vector3 currentPosition = this.gameObject.transform.position;
-			spawnedObject = (GameObject) Instantiate (checkObject, currentPosition, Quaternion.identity);
+			Vector2 currentPosition = this.gameObject.transform.position;
+			Vector3 parentPosition = this.gameObject.transform.parent.transform.position;
+			Collider2D[] coll2d = null;
+			coll2d = Physics2D.OverlapBoxAll (currentPosition, boxSize, 0,10);
 
-			this.gameObject.GetComponentInParent<AILerp> ().possibleTargets.Add (spawnedObject);
+			Debug.Log ("coll2d.Length " + coll2d.Length);
+
+			if (coll2d.Length == 0) {
+				spawnedObject = (GameObject)Instantiate (checkObject, currentPosition, Quaternion.identity);
+
+				this.gameObject.GetComponentInParent<AILerp> ().possibleTargets.Add (spawnedObject);
+			}
 		}
 	
 	}
